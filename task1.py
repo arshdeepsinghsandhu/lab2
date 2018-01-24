93d3cf25b560155bd347a6a5232c07aefb0d60b8
arshdeepsinghsandhu <arshsandhu@dal.ca>
2018-01-23 22:56:31 -0400

    task1

---
+++ task1.py
@@ -0,0 +1,16 @@
+import turtle
+t = turtle.Turtle()
+def square():
+    print(t)
+    t.fd(100)
+    t.lt(90)
+    t.fd(100)
+    t.lt(90)
+    t.fd(100)
+    t.lt(90)
+    t.fd(100)
+square()
+turtle.mainloop()
+
+
+
